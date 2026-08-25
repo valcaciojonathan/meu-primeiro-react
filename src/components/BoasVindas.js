6d2b20e0b83b47fa1e2f8f3b113a2d56956409e8
Jonathan Nunes <valcaciojonathan@gmail.com>
2026-08-25 10:51:14 -0300

    Adiciona exercicios React: src/components/BoasVindas.js

---
+++ src/components/BoasVindas.js
@@ -0,0 +1,12 @@
+function BoasVindas() {
+  const nomeAluno = "Jonathan Valcacio";
+
+  return (
+    <section className="card">
+      <h1>Bem-vindo ao React!</h1>
+      <h2>Aluno: {nomeAluno}</h2>
+    </section>
+  );
+}
+
+export default BoasVindas;
